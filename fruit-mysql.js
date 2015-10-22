757f963f416baf458b3fe9db56c5b491b5dc625f
Khalid REHIOUI <Rehioui@gmail.com>
2015-10-22 22:00:30 +0100

    Connection to mysql database

--- fruit-mysql.js
+++ fruit-mysql.js
@@ -1,3 +1,32 @@
 /* بسم الله الرحمن الرحيم */
 
-module.exports = {};
+module.exports = (function () {
+  
+  var mysql   = require('mysql')
+    , config  = {};
+
+ 
+  function DataManager () {
+    
+    this.type = 'mysql';
+    
+    this.connect = function (conf, callBack) {
+      var connection = mysql.createConnection(config = conf);
+      connection.connect();
+      connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+        connection.end();
+        callBack(err);
+      });
+      return this;
+    }
+    
+    this.config = function (conf) {
+      config = conf;
+      return this;
+    }
+    
+  }
+  
+  return new DataManager;
+  
+}());
