402512044ee8e4e9d8558084cc54886acfd3cd99
Khalid REHIOUI <Rehioui@gmail.com>
2015-10-23 07:38:55 +0100

    .insert() method

--- fruit-mysql.js
+++ fruit-mysql.js
@@ -3,26 +3,60 @@
 module.exports = (function () {
   
   var mysql   = require('mysql')
+    , sql     = require('sql-query')
     , config  = {};
 
- 
+  function exec (query, callBack) {
+    var connection = mysql.createConnection(config);
+    connection.connect();
+    connection.query(query, function(err, rows, fields) {
+      connection.end();
+      callBack(err, rows, fields);
+    });
+  }
+  
+  function generateInsertQuery (tableName, data) {
+    var sqlQuery  = sql.Query()
+      , sqlInsert = sqlQuery.insert()
+    if(Array.isArray(data)) {
+      var values = data.slice(0);
+      return values.reduce(function (query, record) {
+        return query + ' , ' + sqlInsert.into(tableName).set(record).build().split('VALUES').pop();
+      }, sqlInsert.into(tableName).set(values.shift()).build())
+    } else {
+      return sqlInsert.into(tableName).set(data).build();
+    }
+  }
+  
   function DataManager () {
     
     this.type = 'mysql';
     
     this.connect = function (conf, callBack) {
-      var connection = mysql.createConnection(config = conf);
-      connection.connect();
-      connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-        connection.end();
+      var config = conf;
+      exec('SELECT 1 + 1 AS solution', function (err) {
         callBack(err);
-      });
+      })
       return this;
     }
     
     this.config = function (conf) {
       config = conf;
       return this;
+    }
+    
+    this.insert = function (tableName, data, callBack) {
+      var query = generateInsertQuery(tableName, data.slice(0));
+      exec(query, function (err, results) {
+        callBack(err, err ? undefined : {
+            result : {
+                success       : true
+              , affectedCount : results.affectedRows
+              , count         : results.affectedRows
+            }
+          , insertedId : [ results.insertId]
+        })
+      });
     }
     
   }
